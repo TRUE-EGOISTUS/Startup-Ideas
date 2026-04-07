4d200f1e08232486f3eb51c364085758cdaffef2
TRUE_EGOISTUS <goldenfreddy6661@gmail.com>
2026-04-07 15:25:25 +0300

    Импортирование файла users.py в главный файл работы сервера

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,13 +4,14 @@ from app.database import engine, Base
 from app.auth import router as auth_router
 import app.models
 from app.tasks import router as tasks_router
-
+from app.users import router as users_router
 
 app = FastAPI(title="Startup Hybrid API", version="1.0.0")
 
 Base.metadata.create_all(bind = engine)
 app.include_router(auth_router)
 app.include_router(tasks_router)
+app.include_router(users_router)
 
 @app.get("/health")
 async def health():
